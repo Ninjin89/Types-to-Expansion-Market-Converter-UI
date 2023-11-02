3874b9af188ae5625a7b17126fc0df464beb9bd9
Ninjin <glahn76@gmail.com>
2023-11-02 03:40:19 +0100

    Create Test.cs

---
+++ Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using RepositoryPattern.repositories;
+using RepositoryPattern.models;
+
+namespace RepositoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test the pattern
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("global.json");
+            string conString = builder.Build().GetConnectionString("DefaultConnection");
+
+            MyContext myContext = MyContextFactory.Create(conString);
+            TreatmentRepository tr = new TreatmentRepository(myContext);
+            foreach (Treatment item in tr.GetAll())
+            {
+                Console.WriteLine(item.Text);
+            }
+            Console.WriteLine(conString);
+        }
+    }
+}
