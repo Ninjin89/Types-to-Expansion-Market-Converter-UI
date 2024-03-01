63ad75335f9ca3dd45cfe9f886a6f09b74b94f24
Ninjin <glahn76@gmail.com>
2024-03-01 15:43:55 +0100

    Update dummy.cs

--- dummy.cs
+++ dummy.cs
@@ -4,5 +4,4 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
-using RepositoryPattern.repositories;
 using RepositoryPattern.models;
